a62ec2ede999f3c3d0a9bad3fc5002dcbfc841d2
GNKN <ganeken@gmail.com>
2016-05-06 02:39:04 +0000

    challenge retry

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: {maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: {case_sensitive: false }
+    validates :area, length: {maximum: 100}
+    validates :profile, length: {maximum: 500}
     has_secure_password
     validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 end
