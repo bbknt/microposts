0cd0578c55f8ea87fbf3b05dbf1006f0e611baa0
GNKN <ganeken@gmail.com>
2016-05-10 15:56:29 +0000

    add user-index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+  
+  def index
+    @users = User.all
+  end
   
   def show
     @user = User.find(params[:id])
@@ -19,6 +24,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.follower_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
   private
   
   def user_params
