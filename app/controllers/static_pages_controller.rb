dc814af0b4dc56e8f3c14fc48370b016c5d68710
GNKN <ganeken@gmail.com>
2016-05-14 05:04:04 +0000

    add pagination

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
     @micropost = current_user.microposts.build
-    @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+    @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).paginate(page: params[:page], :per_page => 15)
     end
   end
 
